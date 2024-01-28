f52b3186cf4bf0162f2dae772f12a9dd85c4fdff
kmkim0713 <157942767+kmkim0713@users.noreply.github.com>
2024-01-28 16:59:08 +0900

    [Bronze V] Title: ??!, Time: 128 ms, Memory: 14272 KB -BaekjoonHub

---
+++ 백준/Bronze/10926. ？？！/？？！.java
@@ -0,0 +1,20 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+public class Main{
+    public static void main(String args[]) throws Exception{
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        String text = st.nextToken();
+
+        bw.write(text + "??!");
+        bw.flush();
+
+    }
+}
