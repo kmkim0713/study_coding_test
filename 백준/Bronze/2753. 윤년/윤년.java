f4870f16a868423263681508fc477abed77a0bf5
kmkim0713 <157942767+kmkim0713@users.noreply.github.com>
2024-01-28 18:09:55 +0900

    [Bronze V] Title: 윤년, Time: 120 ms, Memory: 14292 KB -BaekjoonHub

---
+++ 백준/Bronze/2753. 윤년/윤년.java
@@ -0,0 +1,25 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.StringTokenizer;
+
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = new StringTokenizer(br.readLine());
+
+        int a = Integer.parseInt(st.nextToken());
+
+        if((a%4==0 && a%100!=0) || (a%400 == 0)){
+            bw.write("1");
+        } else {
+            bw.write("0");
+        }
+
+        bw.flush();
+
+    }
+}
