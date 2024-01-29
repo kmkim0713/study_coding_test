aaf81e50e0d99d4a6568c7671e4a09574ea27154
kmkim0713 <157942767+kmkim0713@users.noreply.github.com>
2024-01-29 19:14:03 +0900

    [Bronze V] Title: 아스키 코드, Time: 124 ms, Memory: 14364 KB -BaekjoonHub

---
+++ 백준/Bronze/11654. 아스키 코드/아스키 코드.java
@@ -0,0 +1,26 @@
+import java.io.BufferedReader;
+import java.io.BufferedWriter;
+import java.io.InputStreamReader;
+import java.io.OutputStreamWriter;
+import java.util.ArrayList;
+import java.util.Collections;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String args[]) throws Exception {
+
+        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+        StringTokenizer st = null;
+
+        st = new StringTokenizer(br.readLine());
+
+        char text = st.nextToken().charAt(0);
+        int num = (byte)text;
+
+        bw.write(String.valueOf(num));
+
+        bw.flush();
+
+    }
+}
