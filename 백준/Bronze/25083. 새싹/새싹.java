9dfff70699a51ac06290aff872162fa9e36f8910
kmkim0713 <157942767+kmkim0713@users.noreply.github.com>
2024-01-31 20:53:25 +0900

    [Bronze V] Title: 새싹, Time: 120 ms, Memory: 13864 KB -BaekjoonHub

---
+++ 백준/Bronze/25083. 새싹/새싹.java
@@ -0,0 +1,27 @@
+import java.io.*;
+import java.util.ArrayList;
+import java.util.Iterator;
+import java.util.StringTokenizer;
+
+public class Main {
+    public static void main(String[] args) throws IOException {
+
+        System.out.println("         ,r'\"7\n" +
+                "r`-_   ,'  ,/\n" +
+                " \\. \". L_r'\n" +
+                "   `~\\/\n" +
+                "      |\n" +
+                "      |");
+
+
+
+//        BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
+//        BufferedWriter bw = new BufferedWriter(new OutputStreamWriter(System.out));
+//
+//        StringTokenizer st = null;
+//        bw.write("\n");
+//        bw.flush();
+
+    }
+
+}
